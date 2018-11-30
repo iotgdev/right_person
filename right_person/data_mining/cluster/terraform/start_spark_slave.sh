6606f66d5e1cf109b5a48f76750332388e4ced72
Nathan Oliver <nathan.oliver@iotecglobal.com>
2018-11-30 17:51:19 +0000

    updated executor memeory

--- right_person/data_mining/cluster/terraform/start_spark_slave.sh
+++ right_person/data_mining/cluster/terraform/start_spark_slave.sh
@@ -3,7 +3,7 @@
 
 export SPARK_HOME=/home/ubuntu/spark
 export SPARK_WORKER_MEMORY=$(echo `free -m | awk '/^Mem:/{print $2}'`*0.8 | bc | cut -f1 -d'.' | awk '{print $1"m"}')
-until sudo /home/ubuntu/spark/sbin/start-slave.sh spark://${master_address}:7077 -m $SPARK_WORKER_MEMORY
+until sudo /home/ubuntu/spark/sbin/start-slave.sh spark://${master_address}:7077 -m $SPARK_WORKER_MEMORY --executor-memory $SPARK_WORKER_MEMORY
 do
   sleep 1.0
 done
