9814989bc9473c0e9f3262543e32a048adb5872b
Nathan Oliver <nathan.oliver@iotecglobal.com>
2018-12-13 12:03:08 +0000

    increasing max result size

--- right_person/data_mining/cluster/session.py
+++ right_person/data_mining/cluster/session.py
@@ -47,7 +47,7 @@ def _get_right_person_spark_config(master_ip):
         _config.set('spark.port.maxRetries', '0')
 
         _config.set('spark.rdd.compress', 'True')
-        _config.set('spark.driver.maxResultSize', '1g')
+        _config.set('spark.driver.maxResultSize', '3g')
         _config.set('spark.executor.memory', get_terraform_vars()['slave_max_memory_gb'] + "g")
 
         _config.set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
