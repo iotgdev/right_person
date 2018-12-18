16f28374a1a2af00d7423b9411ac38695678d9b2
Nathan Oliver <nathan.oliver@iotecglobal.com>
2018-12-18 10:12:19 +0000

    remove swap capability

--- right_person/data_mining/cluster/terraform/start_spark_master.sh
+++ right_person/data_mining/cluster/terraform/start_spark_master.sh
@@ -3,4 +3,5 @@
 
 export SPARK_HOME=/home/ubuntu/spark
 export SPARK_WORKER_MEMORY=$(echo `free -m | awk '/^Mem:/{print $2}'`*0.8 | bc | cut -f1 -d'.' | awk '{print $1"m"}')
+swapoff -a
 source /home/ubuntu/spark/sbin/start-master.sh
--- right_person/data_mining/cluster/terraform/start_spark_slave.sh
+++ right_person/data_mining/cluster/terraform/start_spark_slave.sh
@@ -3,6 +3,7 @@
 
 export SPARK_HOME=/home/ubuntu/spark
 export SPARK_WORKER_MEMORY=${spark_max_mem_gb}
+swapoff -a
 until sudo /home/ubuntu/spark/sbin/start-slave.sh spark://${master_address}:7077 -m $SPARK_WORKER_MEMORY
 do
   sleep 1.0
