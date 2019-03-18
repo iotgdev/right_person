51baff6be8bc50f6bf7035cf447119cd10a6a121
Nathan Oliver <nathan.oliver@iotecglobal.com>
2019-03-18 16:16:14 +0000

    renaming test files

---
+++ test/unit/right_person/models/test_core.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from right_person.models.core import RightPersonModel
+
+
+class TestNewModel(unittest.TestCase):
+
+    def test_weights(self):
+        model = RightPersonModel('name', 'account')
+        self.assertIsNone(model.weights)
+
+    def test_downsampling_rate(self):
+        model = RightPersonModel('name', 'account')
+        self.assertEqual(model.downsampling_rate, 1.0)
+
+    def test_intercept(self):
+        model = RightPersonModel('name', 'account')
+        self.assertEqual(model.intercept, 0)
